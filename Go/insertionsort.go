bef6428ea928636a23fd1d7ff1188e8856ad795a
Vimalbharath <vimalbharath21@gmail.com>
2026-07-05 21:02:25 +0530

    insertion sort in go

--- Go/insertionsort.go
+++ Go/insertionsort.go
@@ -5,8 +5,28 @@ import "fmt"
 func main() {
 	fmt.Println("Hello")
 	//var arr=[4] int{2,3,4,5};
-	arr := []int{2, 3, 4, 5}
+	arr := []int{2, 3, 4, 5, 0, 9, 8}
+	insertionsort(arr)
 	for i := 0; i < len(arr); i++ {
 		fmt.Println(arr[i])
 	}
+}
+
+func insertionsort(arr []int) {
+	var n int = len(arr)
+	//fmt.Println(n)
+	for i := 0; i < n; i++ {
+		j := i
+		for j > 0 {
+			if arr[j-1] > arr[j] {
+				swap(arr, j, j-1)
+			}
+			//fmt.Println(i)
+			j = j - 1
+		}
+	}
+}
+
+func swap(arr []int, a int, b int) {
+	arr[a], arr[b] = arr[b], arr[a]
 }
