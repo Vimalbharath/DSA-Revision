5c19dda5c71e12f22217a22e25edecc56fc4eb92
Vimalbharath <vimalbharath21@gmail.com>
2026-07-14 12:50:13 +0530

    vector of subset

--- C++/substring.cpp
+++ C++/substring.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -30,9 +31,31 @@ void combination(string p, string up)
         combination(p.substr(0, i) + ch + p.substr(i), up.substr(1));
     }
 }
+
+void substring2(string p, string up, vector<string> &result)
+{
+    if (up.empty())
+    {
+        // cout << p << "\n";
+        result.push_back(p);
+        cout << result.size();
+        return;
+    }
+
+    substring2(p + up[0], up.substr(1), result);
+    substring2(p, up.substr(1), result);
+}
+
 int main()
 {
-    substring("", "abc");
-    combination("", "abc");
+    // substring("", "abc");
+    vector<string> result;
+    substring2("", "abc", result);
+    cout << result.size();
+    for (const string s : result)
+    {
+        cout << s << " ";
+    }
+    // combination("", "abc");
     return 0;
 }
