758290ff0b2f88f3092a1792ca29b05b54a09b23
Vimalbharath <vimalbharath21@gmail.com>
2026-07-15 15:04:13 +0530

    height balanced

---
+++ C++/AVL.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    int height;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        value = val;
+        height = 1;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+class BinaryTree
+{
+public:
+    Node *root;
+    BinaryTree()
+    {
+        root = nullptr;
+    }
+
+    int height(Node *node)
+    {
+        return (node == nullptr) ? 0 : node->height;
+    }
+
+    Node *insert(int val, Node *node)
+    {
+
+        if (node == nullptr)
+        {
+            return new Node(val);
+        }
+        if (val < node->value)
+        {
+            node->left = insert(val, node->left);
+        }
+        else if (val > node->value)
+        {
+            node->right = insert(val, node->right);
+        }
+        node->height = max(height(node->left), height(node->left)) + 1;
+        return node;
+    }
+
+    bool balanced()
+    {
+        return balanced(root);
+    }
+
+    bool balanced(Node *node)
+    {
+        if (node == nullptr)
+        {
+            return true;
+        }
+        return abs(height(node->left) - height(node->right)) <= 1 && balanced(node->left) && balanced(node->right);
+    }
+
+    void insert(int val)
+    {
+        root = insert(val, root);
+    }
+    void inorder(Node *node)
+    {
+        if (node == nullptr)
+        {
+            return;
+        }
+        inorder(node->left);
+        cout << node->value << "\n";
+        inorder(node->right);
+    }
+    void inorder()
+    {
+        inorder(root);
+    }
+};
+
+int main()
+{
+    // cout << 0;
+    BinaryTree tree = BinaryTree();
+    tree.insert(12);
+    tree.insert(98);
+    tree.insert(99);
+    tree.insert(0);
+    tree.insert(9);
+    tree.insert(1);
+    //  tree.insert(2);
+    tree.inorder();
+
+    cout << "\n"
+         << tree.balanced();
+    int a = 10;
+    int b = 20;
+    int c = max(a, b);
+    // cout << c;
+
+    return 0;
+}
