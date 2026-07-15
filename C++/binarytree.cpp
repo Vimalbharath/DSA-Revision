eeb36d7ff07b6552949a3e8bfd05bfce55765abf
Vimalbharath <vimalbharath21@gmail.com>
2026-07-15 12:41:08 +0530

    corrected own

--- C++/binarytree.cpp
+++ C++/binarytree.cpp
@@ -25,34 +25,50 @@ public:
         root = nullptr;
     }
 
-    Node *insert(int val)
+    Node *insert(int val, Node *node)
     {
-        Node node = Node(val);
-        if (root == nullptr)
+
+        if (node == nullptr)
         {
-            root = &node;
+            Node newNode = Node(val);
+            node = &newNode;
+            return node;
         }
-        node.left = insert(node.left->value);
-        node.right = insert(node.right->value);
-        return &node;
+        node->left = insert(val, node->left);
+
+        node->right = insert(val, node->right);
+        return node;
     }
-    void display()
+
+    void insert(int val)
     {
-        if (root == nullptr)
+        root = insert(val, root);
+    }
+    void inorder(Node *node)
+    {
+        if (node == nullptr)
         {
             return;
         }
-
-        cout << root->value;
+        inorder(node->left);
+        cout << node->value;
+        inorder(node->left);
+    }
+    void inorder()
+    {
+        inorder(root);
     }
 };
 
 int main()
 {
-    // cout << 0;
+    cout << 0;
     BinaryTree tree = BinaryTree();
     tree.insert(123);
     tree.insert(98);
-    tree.display();
+    tree.insert(99);
+    tree.insert(0);
+    tree.insert(9);
+    tree.inorder();
     return 0;
 }
